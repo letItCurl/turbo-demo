d44f64f20a715cf72b43622f07aeec12851bede9
letItCurl <rolandlopez.developer@gmail.com>
2023-03-13 09:20:04 +0800

    Final demo of turbo frames, turbo streams and renders.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,47 @@
 class CommentsController < ApplicationController
   before_action :set_post
 
+  def index
+    @comments = @post.comments
+  end
+
+  def show
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    @comment.update(post_params)
+    respond_to do |format|
+      if @comment.valid?
+        format.html { redirect_to @post }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: "comments/form", locals: {post: @post, comment: @comment}) }
+      end
+    end
+  end
+
   def create
-    @post.comments.create! params.require(:comment).permit(:content)
-    redirect_to @post
+    @comment = @post.comments.new(post_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: "comments/form", locals: {post: @post, comment: @comment}) }
+      end
+    end
   end
 
   private
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def post_params
+    params.require(:comment).permit(:content)
   end
 end
